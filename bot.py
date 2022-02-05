9b5c3d85f37aa85efe1586a99b0a42b98fcaa223
ali-mmagneto <92598622+ali-mmagneto@users.noreply.github.com>
2022-02-05 12:21:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -53,7 +53,7 @@ def main():
     
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(MessageHandler(Filters.text, reply))
+    dp.add_handler(CommandHandler("imdb", reply))
     dp.add_error_handler(error)
 
     #updater.start_webhook(listen="0.0.0.0",
